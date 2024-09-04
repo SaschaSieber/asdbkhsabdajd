dbf96260f19a61849e7b9cf0b2b8b88c2da5c99d
SaschaSieber <sascha.sieber@amotiq.de>
2024-09-04 16:46:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
-from flask import Flask, jsonify, request, send_from_directory, make_response, render_template ,session
+from flask import Flask, jsonify, request, send_from_directory, make_response, render_template
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import scoped_session, sessionmaker
 import psycopg2
 from psycopg2.extras import RealDictCursor
-from psycopg2 import OperationalError
-from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 
 # Configure your PostgreSQL connection details
-DB_HOST = 'dbc95a5.online-server.cloud'
+DB_HOST = '192.168.0.11'
 DB_PORT = 5432
 DB_USER = 'postgres'
 DB_PASSWORD = 'PgresPW#amotIQ'
@@ -17,8 +15,7 @@ DB_NAME = 'planungstool'
 DB_NAME1 = 'gemeinsam'
 
 
-app.secret_key = 'supersecretkey' 
-global currentServer
+
 
 # SQLAlchemy connection string
 DATABASE_URL = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
@@ -28,45 +25,6 @@ DATABASE_URL1 = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_N
 engine= create_engine(DATABASE_URL)
 engine1= create_engine(DATABASE_URL1)
 
-selected_db = None 
-CORS(app)
-
-
-servers = [
-    {"host": "dbc95a5.online-server.cloud", "port": 5432, "user": "postgres", "password": "PgresPW#amotIQ"},
-    {"host": "9a8fdf0.online-server.cloud", "port": 5432, "user": "postgres", "password": "PgresPW#amotIQ"}
- 
-]
-def check_database_exists(host, port, user, password, database_name, timeout=50):
-    try:
-        # Connect to the PostgreSQL server with a timeout
-        conn = psycopg2.connect(
-            host=host,
-            port=port,
-            user=user,
-            password=password,
-            connect_timeout=timeout
-        )
-       
-        cursor = conn.cursor()
-       
-        # Check if database exists
-        query = """
-        SELECT 1
-        FROM pg_database
-        WHERE datname = %s;
-        """
-       
-        cursor.execute(query, (database_name,))
-        exists = cursor.fetchone()
-        cursor.close()
-        conn.close()
-       
-        return exists is not None
- 
-    except OperationalError as e:
-        print(f"OperationalError: {e}")
-        return False
 
 
 
@@ -83,30 +41,19 @@ def script():
 def serve_static(filename):
     return send_from_directory('.', filename)
 
-
-@app.route('/export', methods=['POST'])
-
-def export_route():
-    global selected_db_global
-    data = request.json
-    selected_db_global = data.get('selectedDatabase')
-    print(selected_db_global)
-    session['selected_db'] = selected_db
-    return jsonify({'message': 'Database selected', 'selectedDatabase': selected_db_global})
-
 # Configure scoped session
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
 
 # Function to get a connection to the PostgreSQL database
-def get_db_connection(InputDB,InputHost):
+def get_db_connection():
     conn = psycopg2.connect(
-        host=InputHost,
+        host=DB_HOST,
         port=DB_PORT,
         user=DB_USER,
         password=DB_PASSWORD,
-        dbname=InputDB
+        dbname=DB_NAME
     )
     return conn
 
@@ -119,41 +66,9 @@ def after_request(response):
 
 @app.route('/table/<table_name>', methods=['GET'])
 def get_table_columns(table_name):
-  
     try:
-        for server in servers:
-
-          if check_database_exists(
-            host=server["host"],
-            port=server["port"],
-            user=server["user"],
-            password=server["password"],
-            database_name=selected_db
-        ):
-                
-
-
-                # Connect to the selected database on the correct server
-                conn = psycopg2.connect(
-                    host=server["host"],
-                    port=server["port"],
-                    user=server["user"],
-                    password=server["password"],
-                    dbname=selected_db
-                )
-                cursor = conn.cursor()
-                global currentServer
-                currentServer=server["host"]
-        
-
-        
-       
-               
-        # Use the correct server for connection (assuming 'currentServer' is set correctly)
-        
-        cursor = conn.cursor()
-
-        
+        conn = get_db_connection()
+        cursor = conn.cursor(cursor_factory=RealDictCursor)
         query = """
             SELECT column_name, data_type
             FROM information_schema.columns
@@ -171,7 +86,7 @@ def get_table_columns(table_name):
 @app.route('/uploadlogs/<table_name>', methods=['GET'])
 def get_upload_logs(table_name):
     try:
-        conn = get_db_connection("planungstool", "dbc95a5.online-server.cloud")
+        conn = get_db_connection()
         cursor = conn.cursor(cursor_factory=RealDictCursor)
         query = """
             SELECT username, timestampval
@@ -195,7 +110,7 @@ def tool_info(table_name):
     query = text("""
         SELECT tool_name, web_link, location, tool_info, tool_version, contact_person, "Support_email"
         FROM tbl_tools
-         WHERE tool_name ILIKE :table_name
+        WHERE tool_name = :table_name
     """)
     with engine1.connect() as connection:
         result = connection.execute(query, {'table_name': table_name}).fetchone()
@@ -224,8 +139,5 @@ def view_table(table_name):
     return render_template('view_table.html', table_name=table_name, columns=columns, rows=rows)
 
 
-
-
 if __name__ == '__main__':
     app.run(app)
-
