0a0dc11a966143b053b01d7f0212a29c1ce6a6a5
SaschaSieber <sascha.sieber@amotiq.de>
2024-09-04 17:28:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -290,7 +290,7 @@
     async function fetchData(d) {
         try {
     
-            let response = await fetch(`/table/${encodeURIComponent(d.name)}`);
+            let response = await fetch(`http://192.168.0.10:3000/table/${encodeURIComponent(d.name)}`);
             if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`);
             }
@@ -333,7 +333,7 @@
 
     async function FetToolsInfo(d) {
         try {
-            let response = await fetch('/tool_info/${d.name}`);
+            let response = await fetch(`http://192.168.0.10/tool_info/${d.name}`);
             if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`);
             }
